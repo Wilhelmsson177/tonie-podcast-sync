887b306574a007045bcd277c434472a332518892
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2023-10-17 07:38:45 +0200

    feat: Allow set root folder for dynaconf

--- tonie_podcast_sync/cli.py
+++ tonie_podcast_sync/cli.py
@@ -1,4 +1,5 @@
 """The command line interface module for the tonie-podcast-sync."""
+import contextlib
 from pathlib import Path
 
 import tomli_w
@@ -7,7 +8,7 @@ from rich.console import Console
 from rich.prompt import Confirm, IntPrompt, Prompt
 from typer import Typer
 
-from tonie_podcast_sync.config import APP_SETTINGS_DIR, settings
+from tonie_podcast_sync.config import settings
 from tonie_podcast_sync.podcast import Podcast
 from tonie_podcast_sync.toniepodcastsync import MAXIMUM_TONIE_MINUTES, ToniePodcastSync
 
@@ -21,8 +22,8 @@ def update_tonies() -> None:
         tps = ToniePodcastSync(settings.TONIE_CLOUD_ACCESS.USERNAME, settings.TONIE_CLOUD_ACCESS.PASSWORD)
     except BoxError:
         Console().print(
-            "There was an error getting the username or password. Please create the settings file or setting the",
-            "environment variables. TPS_TONIE_CLOUD_ACCESS_USERNAME and TPS_TONIE_CLOUD_ACCESS_PASSWORD.",
+            "There was an error getting the username or password. Please create the settings file or set the",
+            "environment variables TPS_TONIE_CLOUD_ACCESS_USERNAME and TPS_TONIE_CLOUD_ACCESS_PASSWORD.",
         )
         return
     for ct_key, ct_value in settings.CREATIVE_TONIES.items():
@@ -32,23 +33,19 @@ def update_tonies() -> None:
 @app.command()
 def create_settings_file() -> None:
     """Create a settings file in your user home."""
-    user_name = Prompt.ask("Enter your Tonie CloudAPI username")
-    password = Prompt.ask("Enter your password for Tonie CloudAPI", password=True)
-    save_login = Confirm.ask("Do you want to save your login data in a .secrets.toml file")
+    app_settings_dir = Path(settings.root_path_for_dynaconf)
+    app_settings_dir.mkdir(parents=True, exist_ok=True)
 
-    if save_login:
-        APP_SETTINGS_DIR.mkdir(parents=True, exist_ok=True)
-        with Path(APP_SETTINGS_DIR / ".secrets.toml").open("wb") as _fs:
-            tomli_w.dump({"tonie_cloud_access": {"username": user_name, "password": password}}, _fs)
+    _handle_tonie_cloud_access(app_settings_dir)
 
-    tps = ToniePodcastSync(user=user_name, pwd=password)
+    tps = ToniePodcastSync(user=settings.TONIE_CLOUD_ACCESS.USERNAME, pwd=settings.TONIE_CLOUD_ACCESS.PASSWORD)
 
     tonies = tps.get_tonies()
     data = {}
 
     for tonie in tonies:
         podcast = Prompt.ask(
-            f"Which podcast do you want to set for Tonie {tonie.name} with ID {tonie.id}?\n"
+            f"Which podcast do you want to set for Tonie '{tonie.name}' with ID '{tonie.id}'?\n"
             "Please enter the URL to the podcast, or leave empty if you don't want to set it.",
         )
         if podcast:
@@ -73,8 +70,31 @@ def create_settings_file() -> None:
                 )
                 data[tonie.id]["maximum_length"] = MAXIMUM_TONIE_MINUTES
 
-    with Path(APP_SETTINGS_DIR / "settings.toml").open("wb") as _fs:
+    with Path(app_settings_dir / "settings.toml").open("wb") as _fs:
         tomli_w.dump({"creative_tonies": data}, _fs)
+
+
+def _handle_tonie_cloud_access(app_settings_dir: Path) -> None:
+    override_secrets = True
+    if settings.get("tonie_cloud_access.username") and settings.get("tonie_cloud_access.password"):
+        override_secrets = Confirm.ask(
+            f"Password and Username ({settings.get('tonie_cloud_access.username')}) already exist."
+            "Do you want to set new values?",
+            default=False,
+        )
+
+    if override_secrets:
+        user_name = Prompt.ask(
+            "Enter your Tonie CloudAPI username",
+            default=settings.get("tonie_cloud_access.username"),
+        )
+        password = Prompt.ask("Enter your password for Tonie CloudAPI", password=True)
+        save_login = Confirm.ask("Do you want to save your login data in a .secrets.toml file")
+
+        if save_login:
+            with Path(app_settings_dir / ".secrets.toml").open("wb") as _fs:
+                tomli_w.dump({"tonie_cloud_access": {"username": user_name, "password": password}}, _fs)
+        settings.update({"tonie_cloud_access": {"username": user_name, "password": password}})
 
 
 if __name__ == "__main__":
